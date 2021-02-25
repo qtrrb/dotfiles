8b3aab6bfc8cc161d6e2479132a712ad25385e26
Quentin <qtorroba@gmail.com>
2021-02-25 15:47:12 +0100

    updated dots.sh

--- dots.sh
+++ dots.sh
@@ -1,5 +1,11 @@
 #!/bin/zsh
+
+if [[ "$OSTYPE" != "darwin20.0" ]]; then
+  echo "\033[1;31mERROR:\033[0m Unsupported OS"
+  exit 1
+fi
 cd ~/dotfiles/ 
 for filename in .*(.); do
   cp $filename ~/
 done
+echo "⚡done"
