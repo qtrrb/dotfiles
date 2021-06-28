8491ab4bd03de2f95ba3cb62563f253182685edc
qtrrb <qtorroba@gmail.com>
2021-06-28 16:36:56 +0200

    put the mac config in a mac folder

--- dots.sh
+++ dots.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
-if [[ "$OSTYPE" != "darwin20.0" ]]; then
-  echo "\033[1;31mERROR:\033[0m Unsupported OS"
-  exit 1
-fi
-cd ~/dotfiles/ 
-for filename in .*(.); do
-  cp $filename ~/
-done
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	cd ~/dotfiles/mac/ 
+	for filename in .*(.); do
+		cp $filename ~/
+	done
 echo "⚡done"
+else
+	echo "\033[1;31mERROR:\033[0m Unsupported OS"
+	exit 1
+fi
+ 
